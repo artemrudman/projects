7f5d3549d80a8801c8623dba1751ced7c022be55
ArtemRudman <artemrudman1994@gmail.com>
2022-09-05 14:38:35 +0300

    Edit errors by 6061 port

--- source/server.ts
+++ source/server.ts
@@ -2,7 +2,7 @@
 import http from 'http';
 import express, { Express } from 'express';
 import morgan from 'morgan';
-import routes from './routes/demo.routes';
+import demoRoutes from './routes/demo.routes';
 
 const router: Express = express();
 
@@ -28,7 +28,7 @@ router.use((req, res, next) => {
 });
 
 /** Routes */
-router.use('/', routes);
+router.use('/demo', demoRoutes.router);
 
 /** Error handling */
 router.use((req, res, next) => {
@@ -40,7 +40,7 @@ router.use((req, res, next) => {
 
 /** Server */
 const httpServer = http.createServer(router);
-const PORT: any = process.env.PORT ?? 6060;
+const PORT: any = process.env.PORT ?? 6061;
 httpServer.listen(
     PORT,
      () => {
